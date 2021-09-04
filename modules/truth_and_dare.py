5291060cc1da3d18cb6645f0af9eb5763d7692aa
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-04 20:58:49 +0530

    Add files via upload

---
+++ modules/truth_and_dare.py
@@ -0,0 +1,19 @@
+import random
+
+from telegram import Update
+from telegram.ext import CallbackContext, run_async
+
+import modules.truth_and_dare_string as truth_and_dare_string
+
+
+@Client.on_message(command(["truth", f"truth@{BOT_USERNAME}"]))
+def truth(update: Update, context: CallbackContext):
+    context.args
+    update.effective_message.reply_text(random.choice(truth_and_dare_string.TRUTH))
+
+
+@Client.on_message(command(["dare", f"dare@{BOT_USERNAME}"]))
+def dare(update: Update, context: CallbackContext):
+    context.args
+    update.effective_message.reply_text(random.choice(truth_and_dare_string.DARE))
+
